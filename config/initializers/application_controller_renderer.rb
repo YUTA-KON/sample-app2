75432c4fcbcbe8590e518f24d0247287a63b420e
YUTA-KON <dorata.1007@gmail.com>
2021-06-03 23:24:57 +0900

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5f5852cc0ca6b4444586da4d5d9deb2125097faec963029593662be71384d9809157962dba39104bcd63ea303d41daa67bf7f91a0e55363fd413e7f0fc1c323b'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_014054) do
+ActiveRecord::Schema.define(version: 2021_06_03_134120) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_29_014054) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "image_id"
   end
 
 end
